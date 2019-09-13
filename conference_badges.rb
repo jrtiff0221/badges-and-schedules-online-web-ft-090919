ce7b3ecf269026a6745ada652b51a3e4a3e18e14
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-09-13 20:09:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  return names.collect { |name| badge_maker(name) }
+end
+
+def assign_rooms(names)
+  room_assignments = []
+  names.each_with_index { |name, index| room_assignments << "Hello, #{name}! You'll be assigned to room #{index + 1}!" }
+  return room_assignments
+end
+def printer
